198ac5c0ba003a9ee9a644b2d0a2c1abdaa43187
naveen@example.com <naveen@example.com>
2022-08-31 09:32:18 +0000

    flask app

---
+++ app.py
@@ -0,0 +1,12 @@
+from flask import Flask
+  
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    return "hi"
+if __name__ == '__main__':
+
+    # run() method of Flask class runs the application
+    # on the local development server.
+    app.run(host='0.0.0.0',port=8080)
